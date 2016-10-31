8b6b3c58e09f207eaec47ba3510b8190d1d769b4
Alexander Gerbl <gerbl.alexander@gmail.com>
2016-10-31 15:03:51 +0100

    Types.hpp: used std::min from algorithm

--- Types.hpp
+++ Types.hpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <ostream>
 #include <cassert>
+#include <algorithm>
 //---------------------------------------------------------------------------
 // HyPer
 // (c) Thomas Neumann 2010
@@ -110,7 +111,7 @@ template <unsigned maxLen> uint64_t Varchar<maxLen>::hash() const
 template <unsigned maxLen> bool Varchar<maxLen>::operator<(const Varchar& other) const
 // Comparison
 {
-   int c=memcmp(value,other.value,min(len,other.len));
+   int c=memcmp(value,other.value,std::min(len,other.len));
    if (c<0) return true;
    if (c>0) return false;
    return len<other.len;
