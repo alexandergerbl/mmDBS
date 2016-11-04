12be952796798b7909f7d1a20c38aed3f5750e44
Alexander Gerbl <gerbl.alexander@gmail.com>
2016-11-04 23:29:41 +0100

    safe before testing SIMD-stuff

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 #include <utility>
 
 
-Numeric<12, 2> hashJoin2(DatabaseColumn& db)
+Numeric<12, 2> hashJoin(DatabaseColumn& db)
 {       
     
-    std::map<std::tuple<Integer, Integer, Integer>, std::pair<Tid, Tid>> customer_HT;
+    std::unordered_map<std::tuple<Integer, Integer, Integer>, std::pair<Tid, Tid>, IntIntIntHash> customer_HT;
     
     for(auto tid = 0; tid < db.m_customer.size(); tid++)
     {
@@ -27,7 +27,7 @@ Numeric<12, 2> hashJoin2(DatabaseColumn& db)
     
     
         //std::pair<tid of customer, tid of order>
-    std::map<std::tuple<Integer, Integer, Integer>, std::pair<Tid, Tid>> resultJoin1;    
+    std::unordered_map<std::tuple<Integer, Integer, Integer>, std::pair<Tid, Tid>, IntIntIntHash> resultJoin1;    
     
     for(auto tid = 0; tid < db.m_order.size(); tid++)
     {
@@ -72,80 +72,19 @@ Numeric<12, 2> hashJoin2(DatabaseColumn& db)
     }
 
     
-    
     return sum;
 
 
 }
 
-Numeric<12, 2> hashJoin(DatabaseColumn& db)
-{
-    
-    std::map<std::tuple<Integer, Integer, Integer>, std::pair<Tid, Tid>> customer_HT;
-    
-    for(auto tid = 0; tid < db.m_customer.size(); tid++)
-    {
-        auto c_last_tmp = db.m_customer.c_last()[tid];
-        
-        if(c_last_tmp.value[0] == 'B')
-        {
-            auto c_id = db.m_customer.c_id()[tid];
-            auto c_d_id = db.m_customer.c_d_id()[tid];
-            auto c_w_id = db.m_customer.c_w_id()[tid];
-            
-            customer_HT.emplace(std::make_tuple(c_d_id, c_w_id, c_id), std::make_pair(tid, 0));
-        }
-    }
-    
-    //std::pair<tid of customer, tid of order>
-    
-    
-    for(auto tid = 0; tid < db.m_order.size(); tid++)
-    {
-        auto o_c_id = db.m_order.o_c_id()[tid];
-        auto o_d_id = db.m_order.o_d_id()[tid];
-        auto o_w_id = db.m_order.o_w_id()[tid];
-        
-        auto tid_customer_it = customer_HT.find(std::make_tuple(o_d_id, o_w_id, o_c_id));
-        
-        if(tid_customer_it != customer_HT.end())
-            tid_customer_it->second.second = tid; 
-    }
-    
-    //second join
-    Numeric<12, 2> sum{0};
-    
-    for(auto ol_tid = 0; ol_tid < db.m_orderline.size(); ol_tid++)
-    {
-        //get range of possible fitting keys
-        auto key_min = std::make_tuple(db.m_orderline.ol_d_id()[ol_tid], db.m_orderline.ol_w_id()[ol_tid], INT32_MIN);
-        auto key_max = std::make_tuple(db.m_orderline.ol_d_id()[ol_tid], db.m_orderline.ol_w_id()[ol_tid], INT32_MAX);
-        
-        auto start = customer_HT.lower_bound(key_min);
-        auto end = customer_HT.upper_bound(key_max);
-        
-        for(auto it = start; it != end; it++)
-        {
-            //check for all tids, whether ol_o_id == o_id
-            if(db.m_orderline.ol_o_id()[ol_tid] == db.m_order.o_id()[it->second.second])
-            {
-                // sum += ol_quality * ol_amount - c_balance*o_ol_cnt
-                auto a = db.m_orderline.ol_quantity()[ol_tid].castP2().castS<12>() * db.m_orderline.ol_amount()[ol_tid].castS<12>();
-                auto b = db.m_customer.c_balance()[it->second.first] * db.m_order.o_ol_cnt()[it->second.second].castS<12>().castP2();
-             
-                sum = sum + a.castM2<12>() - b.castM2<12>();
-            }
-        }
-    }
-    return sum;
-}
+
 
 int main()
 {
  
 	DatabaseColumn db;
     
-    int numRepeat = 1;
+    int numRepeat = 10;
     
     //auto result = hashJoin(db);
     
@@ -156,7 +95,7 @@ int main()
     {
         start = std::chrono::system_clock::now();
     
-        auto result = hashJoin2(db);
+        auto result = hashJoin(db);
         
         end = std::chrono::system_clock::now();
         std::cout << i << "\t" << result << "\t" << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << " µs/hashjoin\n" << std::endl;
